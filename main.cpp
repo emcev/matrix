c26b05e3ae54c663828483d20ace85d4f7d2a894
Артур <32384736+BulatovArthur@users.noreply.github.com>
2018-04-22 02:43:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,40 @@
-#include <matrix.hpp> 
+#include <matrix.hpp>
 
 int main() {
-	Matrix <int> m1{3,5};
-    	Matrix <int> m2{1, 2, 3, 4, 5, 6};
-    	Matrix <int> m3{m2};
+        Matrix <int, 2, 1> m1{3,5};
+        Matrix <int, 3, 2> m2{1, 2, 3, 4, 5, 6};
+        Matrix <int, 3, 2> m3{m2};
 
-    	cout << m1 << endl;
-    	cout << m2 << endl;
-    	cout << m3 << endl;
+        cout << m1 << endl;
+        cout << m2 << endl;
+        cout << m3 << endl;
 
-    	Matrix <int> m4;
-    	m4 = m3;
-    	cout << m4 << endl;
+        Matrix <int, 3, 2> m4;
+        m4 = m3;
+        cout << m4 << endl;
 
-    	m4[0][0] = 8;
-    	cout << m4 << endl;
+        m4[0][0] = 8;
+        cout << m4 << endl;
 
-    	cout << boolalpha  << (m2 == m3) << (m3 == m4) << endl;
+        cout << boolalpha  << (m2 == m3) << (m3 == m4) << endl;
 
-    	cout << m1.rows() << m1.columns() << endl;
+        cout << m1.rows() << m1.columns() << endl;
 
-    	Matrix <int> m5{0, 2, 4, 6, 8, 10};
-    	cout << m3 + m5 << endl;
-    	cout << m3 - m5 << endl;
+        Matrix <int, 3, 2> m5{0, 2, 4, 6, 8, 10};
+        cout << m3 + m5 << endl;
+        cout << m3 - m5 << endl;
 
-    	Matrix <int> m6{};
-    	cout << m6.empty() << endl;
+        Matrix <int, 3, 2> m6{};
+        cout << m6.empty() << endl;
 
-    	ofstream out{"matrix.bin"};
-    	out << m3;
+        ofstream out{"matrix.bin"};
+        out << m3;
 
-    	ifstream in{"matrix.bin"};
-    	in >> m6;
+        ifstream in{"matrix.bin"};
+        in >> m6;
 
-    	m3.m_swap(m6);
+        m3.m_swap(m6);
 
-    	cout << m3 << endl;
+        cout << m3 << endl;
 }
+
