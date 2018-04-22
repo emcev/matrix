5fdaab8d3e22b3c19b4d93e480fe477046605843
Артур <32384736+BulatovArthur@users.noreply.github.com>
2018-04-22 17:11:36 +0300

    Update matrix.hpp

--- matrix.hpp
+++ matrix.hpp
@@ -11,49 +11,46 @@ public:
     Matrix();
     Matrix(initializer_list<type> il);
     Matrix(const Matrix&);
-    auto m_swap(Matrix&) -> void;
+    auto swap(Matrix&) -> void;
     auto operator=(const Matrix& q) -> Matrix&;
     auto empty() -> bool;
     auto columns() -> size_t;
     auto rows() -> size_t;
-    auto operator-(const Matrix&) -> Matrix&;
-    auto operator+(const Matrix&) -> Matrix&;
+    auto operator-(const Matrix&) -> Matrix;
+    auto operator+(const Matrix&) -> Matrix;
     auto operator[](size_t index) -> type*;
     bool operator==(const Matrix&);
     template <typename T2, size_t N2, size_t M2>
-    friend ostream& operator<<(ostream&, Matrix<T2, N2, M2>&);
+    friend ostream& operator<<(ostream&, const Matrix<T2, N2, M2>&);
     template <typename T3, size_t N3, size_t M3>
     friend istream& operator>>(istream&, Matrix<T3, N3, M3>&);
     ~Matrix();
-    size_t n = N;
-    size_t m = M;
 private:
     type** ptr;
 };
 
 template <typename type, size_t N, size_t M>
-Matrix<type, N, M>::Matrix() : ptr(nullptr), n(N), m(M) {
-    n = m = 0;
+Matrix<type, N, M>::Matrix() : ptr(nullptr) {
 }
 
 template <typename type, size_t N, size_t M>
-Matrix<type, N, M>::Matrix(initializer_list<int> il) {
-    ptr = new int*[n];
+Matrix<type, N, M>::Matrix(initializer_list<type> il) {
+    ptr = new type*[N];
     auto p = il.begin();
-    for (int i = 0; i < n; i++)
-        ptr[i] = new int[m];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++, p++)
+    for (unsigned int i = 0; i < N; i++)
+        ptr[i] = new type[M];
+    for (unsigned int i = 0; i < N; i++) {
+        for (unsigned int j = 0; j < M; j++, p++)
             ptr[i][j] = *p;
     }
 }
 
 template <typename type, size_t N, size_t M>
 Matrix<type, N, M>::Matrix(const Matrix& m_matrix) {
-    ptr = new int *[m_matrix.n];
-    for (int i = 0; i < m_matrix.n; i++){
-        ptr[i] = new int[m_matrix.m];
-        for (int j = 0; j < m_matrix.m; j++)
+    ptr = new type *[N];
+    for (unsigned int i = 0; i < N; i++){
+        ptr[i] = new type[M];
+        for (unsigned int j = 0; j < M; j++)
             ptr[i][j] = m_matrix.ptr[i][j];
     }
 }
@@ -68,64 +65,53 @@ auto Matrix<type, N, M>::empty() -> bool {
 
 template <typename type, size_t N, size_t M>
 auto Matrix<type, N, M>::columns() -> size_t {
-    return m;
+    return M;
 }
 
 template <typename type, size_t N, size_t M>
 auto Matrix<type, N, M>::rows() -> size_t {
-    return n;
+    return N;
 }
 
 template <typename type, size_t N, size_t M>
-auto Matrix<type, N, M>::operator[](size_t index) -> int* {
+auto Matrix<type, N, M>::operator[](size_t index) -> type* {
     return ptr[index];
 }
 
 template <typename type, size_t N, size_t M>
-auto Matrix<type, N, M>::operator-(const Matrix& m_matrix) -> Matrix& {
-    if (n == m_matrix.n && m == m_matrix.m){
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++)
-                ptr[i][j] = ptr[i][j] - m_matrix.ptr[i][j];
-        }
-        return *this;
+auto Matrix<type, N, M>::operator-(const Matrix& m_matrix) -> Matrix {
+    for (unsigned int i = 0; i < N; i++){
+        for (unsigned int j = 0; j < M; j++)
+            ptr[i][j] = ptr[i][j] - m_matrix.ptr[i][j];
     }
-    else
-        cout << "Error" << endl;
+    return *this;
 }
 
 template <typename type, size_t N, size_t M>
-auto Matrix<type, N, M>::operator+(const Matrix& m_matrix) -> Matrix& {
-    if (n == m_matrix.n && m == m_matrix.m){
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++)
-                ptr[i][j] = ptr[i][j] + m_matrix.ptr[i][j];
-        }
-        return *this;
+auto Matrix<type, N, M>::operator+(const Matrix& m_matrix) -> Matrix {
+    for (unsigned int i = 0; i < N; i++){
+        for (unsigned int j = 0; j < M; j++)
+            ptr[i][j] = ptr[i][j] + m_matrix.ptr[i][j];
     }
-    else
-        cout << "Error" << endl;
+    return *this;
 }
 
 template <typename type, size_t N, size_t M>
 bool Matrix<type, N, M>::operator==(const Matrix& m_matrix) {
-    if (n == m_matrix.n && m == m_matrix.m){
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (ptr[i][j] != m_matrix.ptr[i][j])
-                    return false;
-            }
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            if (ptr[i][j] != m_matrix.ptr[i][j])
+                return false;
         }
-        return true;
     }
-    return false;
+    return true;
 }
 
 template <typename type, size_t N, size_t M>
-ostream& operator<<(ostream& os, Matrix<type, N, M>& m_matrix) {
-    os << m_matrix.n << " " << m_matrix.m << endl;
-    for (int i = 0; i < m_matrix.n; i++){
-        for (int j = 0; j < m_matrix.m; j++)
+ostream& operator<<(ostream& os, const Matrix<type, N, M>& m_matrix) {
+    os << N << " " << M << endl;
+    for (unsigned int i = 0; i < N; i++){
+        for (unsigned int j = 0; j < M; j++)
             os << m_matrix.ptr[i][j]<<"  ";
         os << endl;
     }
@@ -135,12 +121,11 @@ ostream& operator<<(ostream& os, Matrix<type, N, M>& m_matrix) {
 template <typename type, size_t N, size_t M>
 istream& operator>>(istream& is, Matrix<type, N, M>& m_matrix) {
     if(is) {
-        is >> m_matrix.n >> m_matrix.m;
-        m_matrix.ptr=new int*[m_matrix.n];
-        for(int i=0; i < m_matrix.n; i++)
-            m_matrix.ptr[i]=new int[m_matrix.m];
-        for (int i = 0; i < m_matrix.n; i++) {
-            for (int j = 0; j < m_matrix.m; j++)
+        m_matrix.ptr = new type*[N];
+        for(unsigned int i = 0; i < N; i++)
+            m_matrix.ptr[i] = new type[M];
+        for (unsigned int i = 0; i < N; i++) {
+            for (unsigned int j = 0; j < M; j++)
                 is >> m_matrix.ptr[i][j];
         }
     }
@@ -148,36 +133,48 @@ istream& operator>>(istream& is, Matrix<type, N, M>& m_matrix) {
 }
 
 template <typename type, size_t N, size_t M>
-auto Matrix<type, N, M>::m_swap(Matrix& m_matrix) -> void {
-    swap(ptr, m_matrix.ptr);
-    swap(n, m_matrix.n);
-    swap(m, m_matrix.m);
+auto Matrix<type, N, M>::swap(Matrix& m_matrix) -> void {
+    type **curr;
+    curr = new type *[N];
+    for (unsigned int i = 0; i < N; i++) {
+        curr[i] = new type[M];
+        for (unsigned int j = 0; j < M; j++)
+            curr[i][j] = m_matrix.ptr[i][j];
+    }
+    for (unsigned int i = 0; i < N; i++) {
+        for(unsigned int j = 0; j < M; j++)
+            m_matrix.ptr[i][j] = this->ptr[i][j];
+    }
+    for (unsigned int i = 0; i < N; i++) {
+        for(unsigned int j = 0; j < M; j++)
+            this->ptr[i][j] = curr[i][j];
+    }
+    for (unsigned int i = 0; i < N; i++)
+        delete[] curr[i];
+    delete [] curr;
 }
 
 template <typename type, size_t N, size_t M>
 auto Matrix<type, N, M>::operator=(const Matrix& m_matrix) -> Matrix& {
     if (ptr != nullptr) {
-        for (int i = 0; i < n; i++)
+        for (unsigned int i = 0; i < N; i++)
             delete[] ptr[i];
         delete [] ptr;
         return *this;
     }
-    n = m_matrix.n;
-    m = m_matrix.m;
-    ptr = new int*[n];
-    for (int i = 0; i < n; i++)
-        ptr[i] = new int[m];
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
+    ptr = new type*[N];
+    for (unsigned int i = 0; i < N; i++)
+        ptr[i] = new type[M];
+    for (unsigned int i = 0; i < N; i++){
+        for (unsigned int j = 0; j < M; j++)
             ptr[i][j] = m_matrix.ptr[i][j];
-        }
     }
     return *this;
 }
 
 template <typename type, size_t N, size_t M>
 Matrix<type, N, M>::~Matrix() {
-    for (int i = 0; i < n; i++)
+    for (unsigned int i = 0; i < N; i++)
         delete[] ptr[i];
     delete [] ptr;
 }
